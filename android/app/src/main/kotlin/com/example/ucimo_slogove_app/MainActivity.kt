85d62636ad78011c31997f37cc7b3ebed5cc7e54
lara-marcec <laramarcec@gmail.com>
2025-10-23 22:53:58 +0200

    add android

---
+++ android/app/src/main/kotlin/com/example/ucimo_slogove_app/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.ucimo_slogove_app
+
+import android.os.Bundle
+import com.xraph.plugin.flutter_unity_widget.FlutterUnityActivity
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterUnityActivity() {
+}
